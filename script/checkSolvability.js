05342f958f23aa1ca8b1556cac7f692b553899be
LucianABC <louabecasis@gmail.com>
2020-02-15 20:38:26 -0300

    liiiiittle refactor to calcInversions

--- script/checkSolvability.js
+++ script/checkSolvability.js
@@ -1,16 +1,17 @@
-const calcInversions=(pieceNum, x,y)=>{
+const calcInversions=(pieceNum, pieceX,pieceY)=>{
     let inversions = 0;
     let lastTile = rows * rows;
-    let position = positionMap[y][x];
-    let positionNumber = y*rows+x;
+    let position = positionMap[pieceY][pieceX];
+    let positionNumber = pieceY*rows+pieceX;
     
     //Recorre el las posiciones x sus respectivos numeros a partir de la currentposition
     for (let i = positionNumber + 1; i < lastTile; ++i) {
-      let k = i % rows;
-      let l = Math.floor(i / rows);
+      //Calculates the coordinates of the next piece
+      let y = i % rows;
+      let x = Math.floor(i / rows);
   
-      let compValue = positionMap[k][l].id;
-      if (pieceNum > compValue && pieceNum != (lastTile - 1)) {
+      let compValue = positionMap[y][x].id;
+      if ((pieceNum > compValue) && (pieceNum != lastTile-1)) {
         ++inversions;
       }
     }
@@ -21,11 +22,14 @@ const totalInversions=()=>{
     let inversions = 0;
     for (let y = 0; y < rows; ++y) {
       for (let x = 0; x < rows; ++x) {
+        
         let pieceNum = positionMap[y][x].id;
-        inversions += calcInversions(pieceNum, x, y);
+        if(pieceNum != "empty"){
+          
+          inversions += calcInversions(pieceNum, x, y);
+        }
       }
     }
-    console.log(inversions)
     return inversions;
 };
 
